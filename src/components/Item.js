66f1008eed53e96287df61c49e56da6fa160c0e9
+dennisngangairungu <+>
2023-11-16 21:09:51 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  //console.log(category)
+  const [none ,setNone] = useState('')
+  const [names,setName]=useState("Add to Cart")
+  
+  function handleClick2(){
+    
+    setNone(none === ''? 'in-cart':"")
+    setName(names ==="Add to Cart"?  "Remove From Cart" : "Add to Cart")
+    
+  }
+  //const appClass = none ? "App dark" : "App light"
   return (
-    <li className="">
+    <li className={none}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button  onClick={handleClick2} className="add">{names}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React ,{useState}from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+ //console.log(items)
+ //const [one, setone] =useState(items)
+  const [filters, filterBy]= useState(items.category)
+
+  function handleFilter(event){
+   // filterBy()
+//console.log(event.target.value)
+    const foodsToDisplay = items.filter(console.log(items));
+    console.log(foodsToDisplay)
+  
+  }
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+        <select onClick={handleFilter} name="filter">
+          <option  value="All">Filter by category</option>
+          <option   value="Produce">Produce</option>
+          <option   value="Dairy">Dairy</option>
+          <option   value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
